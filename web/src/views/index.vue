75c407f00584259d70e3b39951274d0fa1947a84
yangwb1123 <1832227346@qq.com>
2022-05-13 17:09:21 +0800

    update

--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -1,19 +1,41 @@
 <template>
   <div>
-    <van-button type="primary" @click="handleClick">录入</van-button>
+    <van-grid>
+      <van-grid-item icon="user-o" text="登录" @click="$router.push('/scan')" />
+      <van-grid-item icon="qr" text="扫码" @click="$router.push('/scan')" />
+    </van-grid>
+    <van-tag
+      size="medium"
+      type="warning"
+      style="width: 100vw;
+            height: 10vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            "
+    >
+      <span>未登录，无法连接到数据库</span>
+      <span>未登录，未绑定工号</span>
+    </van-tag>
   </div>
 </template>
 
 <script>
-import { Button } from 'vant'
+import { Button, Grid, Tag, GridItem } from 'vant'
 
 export default {
   components: {
-    [Button.name]: Button
+    [Button.name]: Button,
+    [Tag.name]: Tag,
+    [Grid.name]: Grid,
+    [GridItem.name]: GridItem
   },
-  methods: {
-    handleClick () {
-      this.$router.push('/scan')
+  data () {
+    return {}
+  },
+  computed: {
+    isConnected () {
+      return ''
     }
   }
 }
